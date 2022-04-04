75e314916d1350da780eca2950b7a35b003f3c52
geosarr <83412621+geosarr@users.noreply.github.com>
2022-04-04 23:08:28 +0200

    changed multi-indexing to uni-indexing v2

--- collection.py
+++ collection.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass # practical for object oriented coding have a look here https://docs.python.org/3/library/dataclasses.html
 from tqdm import tqdm
 
-def index_collection(documents, indices):
+def index_collection(documents, index):
     '''
     # INPUT:     ## documents is the collection of documents 
                  ## index is a type of index like inverted index, positional index
