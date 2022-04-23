52237e42420bdd015f5db4c9c584c84c1f8bbc3f
SARR Georges <georges_sarr@dg.carrefour.com>
2022-04-23 04:58:35 +0200

    added benchmark function

--- evaluation.py
+++ evaluation.py
@@ -25,12 +25,49 @@ def MRR(model, judgments, at_K=10):
     '''
     SRR=0
     for query in tqdm(judgments):
-        SRR+=inv_rank_most_relevant_doc(model, query, judgments, at_K) # rank of the most pertinent document
+        SRR+=inv_rank_most_relevant_doc(model, query, judgments, at_K) 
     return SRR/len(judgments) 
 
 
 
-def precision(model, at_K=None):
+def precision(model, query, judgments):
     '''
-    
+    Returns the precision of a given model for a query.
+    It is not precision@K because we do not set a priori the rank K, instead this precision
+    depends on the available jugments for the query
     '''
+    prec=0
+    rel_doc_id=judgments[query].keys()
+    model.top=len(judgments[query]) # to retrieve only a number of documents equal to the number of available judgments
+    results=model.retrieval(query)
+    retrieved_docs=array(results)[:,0] # IDs of the retrieved docs
+    if len(retrieved_docs)>0:
+        prec=sum([ID in retrieved_docs for ID in rel_doc_id])/len(retrieved_docs)
+    return prec
+
+
+def MAP(model, judgments):
+    '''
+    Returns the Mean Average Precision (not MAP@K) of a model given jugments
+    '''
+    SAP=0
+    for query in tqdm(judgments):
+        SAP+=precision(model, query, judgments) 
+    return SAP/len(judgments) 
+
+
+def run_benchmark(benchmark, judgments):
+    '''
+    Model(s) evaluation
+    '''
+    for model_name in benchmark:
+        models=benchmark[model_name]["model"]
+        at_Ks=list(benchmark[model_name]["MRR@"].keys())
+        print(f'Running {len(models)} {model_name} model(s)')
+        for pos, model in enumerate(models):
+            print(f'\t{pos+1} model over {len(models)}')
+            for K in at_Ks:
+                benchmark[model_name]["MRR@"][K].append(MRR(model, judgments, at_K=K))
+            benchmark[model_name]["MAP"].append(MAP(model, judgments))
+    return benchmark
+            
